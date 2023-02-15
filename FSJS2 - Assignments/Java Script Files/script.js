a9a2f1c6b81bbe8a9080be246c6e77024fc2f9f3
Yashoda <yashodalakshmi999@gmail.com>
2023-02-15 22:48:07 +0530

    ja folder updated

--- FSJS2 - Assignments/Java Script Files/script.js
+++ FSJS2 - Assignments/Java Script Files/script.js
@@ -1,6 +1,115 @@
-let x = 10;
-let y = 20;
+// //Print
+// console.log("Hello Yashodaaa");
 
-console.log(x+y);
-console.log(x-y);
-console.log(x*y);
+// //VALUES - Data Types:
+// //Number Values:
+// 7
+// 2.5
+// //String Values:
+// ("Yashoda");
+// ('Sai');
+// //Boolean Values:
+// true;
+// false;
+// //Empty values:
+// null;
+// undefined;
+// //These are all Primitive data types -always have only one single value.
+
+// //Array - 
+// [1, 'sai', true, "Ram"];
+// //0, 1, 2, 3     index
+
+// //object
+// {
+//     key:value;
+// }
+
+// {
+//     fname:'sai',
+//     lname:'ram';
+// }
+//These are all Non-primitive data types - it always have more than one values
+
+//Variables:
+//1.Let
+//2.var
+//3.const
+
+// let firstname = "sai";
+// let lastname = 'ram';
+// const MobileNo = 90000000000;
+
+// //print
+// console.log(firstname);
+// console.log(lastname);
+// console.log(MobileNo);
+
+// console.log('My Name is', firstname + '/ My last name is', lastname + '/ My Mobile Number is', MobileNo);
+
+// NEED TO CHECK:console.log(`my name`, $firstname);
+// , $lastname, $MobileNo
+
+//Operators:
+// Assignment Operators:
+// let x = 10;
+// let y = 15;
+
+//arithmetic Operators:
+// let z = x+y;
+// // console.log(z);
+// console.log(x+y);
+// console.log(x-y);
+// console.log(x*y);
+// console.log(x/y);
+// console.log(x%y);
+
+//comparision operators <,>,==, ===, <=, >=
+
+// let abc = 10;
+// let xyz = 20;
+// console.log(abc<xyz);
+// console.log(abc>xyz);
+// console.log(abc<=xyz);
+// console.log(abc>=xyz);
+// console.log(abc==xyz);
+// console.log(abc===xyz);
+
+// let y;
+// console.log(y);
+
+// 4.DECISIONS OR CONDITIONS(IF)
+
+// let age = 17;
+// if(age >= 18){
+//     console.log('You are allowed');
+// }
+// else{
+// console.log('Not allowed');
+// }
+
+// let gender = 'female';
+// if(gender == 'female'){
+//     console.log('allowed');
+// }
+
+// else{
+// console.log('Not allowed');
+// }
+
+// let STD = 9;
+// if(STD == 1){
+//     console.log('ONE');
+// }
+// else if(STD == 2){
+//     console.log('TWO')
+// }
+// else if(STD == 3){
+//     console.log('THREE')
+// }
+// else if(STD == 4){
+//     console.log('FOUR')
+// }
+// else{
+//     console.log('PRINCIPLE')
+// }
