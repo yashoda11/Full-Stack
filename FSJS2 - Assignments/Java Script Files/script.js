3ae7b0bb0434d7b233bad767993ca743f8dd4ba3
Yashoda <yashodalakshmi999@gmail.com>
2023-02-17 13:47:29 +0530

    javascript file updated

--- FSJS2 - Assignments/Java Script Files/script.js
+++ FSJS2 - Assignments/Java Script Files/script.js
@@ -79,7 +79,7 @@
 // console.log(y);
 
 // 4.DECISIONS OR CONDITIONS(IF)
-
+//if else
 // let age = 17;
 // if(age >= 18){
 //     console.log('You are allowed');
@@ -97,7 +97,9 @@
 // console.log('Not allowed');
 // }
 
-// let STD = 9;
+//if else - example
+
+// let STD = 6;
 // if(STD == 1){
 //     console.log('ONE');
 // }
@@ -113,3 +115,88 @@
 // else{
 //     console.log('PRINCIPLE')
 // }
+
+//Switch Case Example:
+
+// let user = 'sai';
+// switch(user)
+//     {
+//         case 'admin':
+//             console.log('Redirect him to admin page');
+//         break;
+//         case 'mentor':
+//             console.log('redirect him to mentor page');
+//         break;
+//         default:
+//             console.log('I am simple User')
+//     }
+
+
+//3. Ternary Operations:
+// condition?true:false;
+
+// let raining = false;
+// // raining?console.log('Its Raining'):console.log('Its not Raining');
+// let userLogin = false;
+// userLogin?console.log('Logout'):console.log('LOGIN');
+
+//ARRAY:
+// let name = ['ani', 'sai', 'ganesh', 'ram', 'hari'];
+// console.log(name);
+// console.log(name[2]);
+// // OBJECT:
+// let BankAccount = {
+//     fname : 'Sai',
+//     lname : "ram",
+//     mblno : 900000000000,
+//     bal : 900000
+// };
+// console.log(BankAccount);
+// console.log(BankAccount.bal);
+// DATE:
+// const now = new Date();
+// console.log(now);
+// console.log(now.toString());
+// console.log(now.getFullYear());
+// console.log(now.getMonth());
+// console.log(now.getDay());
+// console.log(now.getTime());
+// console.log(now.getHours());
+// console.log(now.getMinutes());
+// console.log(now.getSeconds());
+// MATH
+const PI = Math.PI;
+// console.log(PI);
+// console.log(Math.round(PI));
+// console.log(Math.round(9.8));
+// console.log(Math.round(9.5))
+// console.log(Math.round(9.4));
+// console.log(Math.round(12.1));
+// console.log(Math.floor(12.1));
+// console.log(Math.ceil(12.1));
+// console.log(Math.min(1,2,));
+// console.log(Math.max(1,2,));
+// console.log(Math.random());
+// console.log(Math.random()*10);
+// console.log(Math.random()*100);
+// console.log(Math.round(Math.random()*50));
+// console.log(Math.pow(2,2));
+// console.log(Math.log(2));
+// console.log(Math.sin(90));
+
+// LOOPS:
+// 1.do-while:
+
+// let i=8;
+// do{
+//     console.log('Value of i: ',i);
+//     i++;
+// }while(i<=5);
+
+//2. while:
+// let j=8;
+// while(j<=5){
+//     console.log('J is: ',j);
+//     j++;
+// }
+
