8711989b64e17bdd608d296828f0885b1db16887
Yashoda Viyyapu <121178061+yashoda11@users.noreply.github.com>
2023-05-14 22:43:17 +0530

    Update context.js

--- Practise Projects/JavaScript Practising Codes/Hitesh Sir Short Vedio Codes/context.js
+++ Practise Projects/JavaScript Practising Codes/Hitesh Sir Short Vedio Codes/context.js
@@ -335,6 +335,88 @@ sam.getCourseCount();
 sam.getFirstname();
 //console.log(sam);
 
+var User = function (firstName, courseCount) {
+  this.firstName = firstName;
+  this.courseCount = courseCount;
+  this.getCourseCount = function () {
+    console.log(`Course count is: ${this.courseCount}`);
+  };
+};
+
+User.prototype.getFirstname = function () {
+  console.log(`Your firstname is : ${this.firstName}`);
+};
+
+var hitesh = new User("hitesh", 2);
+hitesh.getCourseCount();
+
+if (hitesh.hasOwnProperty("firstName")) {
+  hitesh.getFirstname();
+}
+
+//console.log(hitesh);
+
+var sam = new User("Sam", 1);
+sam.getCourseCount();
+sam.getFirstname();
+//console.log(sam);
+
+// for (let i = 0; i < 10; i++) {
+//     console.log(i);
+// }
+
+// const myStates1 = ["Rajasthan", "Delhi", 1947, "Assam", "TN", "Maha"];
+// for (let i = 0; i <=myStates1.length; i++) {
+//     console.log(i);
+// }
+
+// const myStates2 = ["Rajasthan", "Delhi", 1947, "Assam", "TN", "Maha"];
+// for (let i = 0; i <=myStates2.length; i++) {
+//     console.log(myStates2[0]);
+// }
+
+// const myStates3 = ["Rajasthan", "Delhi", 1947, "Assam", "TN", "Maha"];
+
+// for (let i = 0; i <myStates3.length; i++) {
+//     console.log(myStates3[i]);
+// }
+
+// for (let i = 0; i < myStates3.length; i++) {
+//     if(typeof myStates3[i] !== "string")
+//        continue;
+//     console.log(myStates3[i]);
+// }
+// for (let i = 0; i < myStates3.length; i++) {
+//     if(typeof myStates3[i] === "string")
+//        continue;
+//     console.log(myStates3[i]);
+// }
+
+// for (let i = 0; i < myStates3.length; i++) {
+//     if(typeof myStates3[i] !== "string")
+//        break;
+//     console.log(myStates3[i]);
+// }
+
+// const myStates = ["Rajasthan", "Delhi", 1947, "Assam", "TN", "Maha"];
+// myStates.forEach ((s) => (console.log(s)));
+
+// const names = ["youtube", "Amazon", "Hai", "Instagram", "facebook"];
+// console.log(names);
+// for (const n of names)
+// console.log(n);
+
+const symbols = {
+    yt: "youtube",
+    fb: "Facebook",
+    id: "Instagram",
+    lco: "Learn Code Online"
+};
+for ( const n in symbols)
+// console.log(n);
+// console.log(symbols[n]);
+console.log( `The Key is: ${n} and its Value is: ${symbols[n]}.`);
+
 
 
 
