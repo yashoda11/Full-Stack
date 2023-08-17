c437c20bfbc1de623975df0795488387aed94576
Yashoda <yashodalakshmi999@gmail.com>
2023-08-17 19:07:09 +0530

    JS NOTES

--- Practise Projects/JavaScript Practising Codes/Java_Script_Notes.js
+++ Practise Projects/JavaScript Practising Codes/Java_Script_Notes.js
@@ -466,3 +466,54 @@
 // console.log(obj3.fly);
 // console.log(obj3.PI);
 // console.log(Object.getPrototypeOf(obj3));
+
+// **************Calculator :
+// function sum(val1, val2){
+//     console.log('Addition of', val1, 'and' ,val2, 'is:',val1+val2);
+// }
+// sum(10, 12);
+// sum(24, 56);
+// sum(56, 14);
+// sum(26, 23);
+// sum(66, 22);
+
+// function sub(val1, val2){
+//     console.log('Subtraction of', val1, 'and' ,val2, 'is:',val1-val2);
+// }
+// sub(32, 22);
+// sub(54, 33);
+// sub(56, 14);
+// sub(26, 13);
+// sub(66, 22);
+// sub(10, 10);
+// sub(20, 20);
+// sub(30, 30);
+
+
+// function mult(val1, val2){
+//     console.log('Multiplication of', val1, 'and' ,val2, 'is:',val1*val2);
+// }
+// mult(32, 22);
+// mult(54, 33);
+// mult(56, 14);
+// mult(26, 13);
+
+
+// function div(val1, val2){
+//     console.log('Division of', val1, 'and' ,val2, 'is:',val1/val2);
+// }
+// div(32, 4);
+// div(54, 3);
+// div(56, 2);
+// div(24, 4);
+// div(66, 6);
+
+
+// function rem(val1, val2){
+//     console.log('Subtraction of', val1, 'and' ,val2, 'is:',val1%val2);
+// }
+// rem(32, 22);
+// rem(54, 33);
+// rem(56, 14);
+// rem(26, 13);
+// rem(66, 22);
