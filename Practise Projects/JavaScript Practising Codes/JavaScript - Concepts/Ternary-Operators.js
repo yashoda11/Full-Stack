e7109413676cd4443972d1ab95ac73449b0441f4
Yashoda <yashodalakshmi999@gmail.com>
2023-09-07 22:05:56 +0530

    Ternary-Operators

---
+++ Practise Projects/JavaScript Practising Codes/JavaScript - Concepts/Ternary-Operators.js
@@ -0,0 +1,9 @@
+//Ternary Operators
+//condition ? true : false;
+
+let authenticated = true;
+let authenticated1 = false;
+
+authenticated?console.log("Success"):console.log("Fail");
+
+authenticated1?console.log("Success"):console.log("Fail");
