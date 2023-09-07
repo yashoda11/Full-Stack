1ce18beb707fa994b9a8e7a69c38066e7940c743
Yashoda <yashodalakshmi999@gmail.com>
2023-09-07 22:17:08 +0530

    Falsy-Values

---
+++ Practise Projects/JavaScript Practising Codes/JavaScript - Concepts/Coercion.js
@@ -0,0 +1,4 @@
+console.log(2+2);
+console.log(2+"2");
+console.log(2+parseInt("2"));
+console.log("2"+"2");
---
+++ Practise Projects/JavaScript Practising Codes/JavaScript - Concepts/Falsy-Values.js
@@ -0,0 +1,29 @@
+console.log("Falsy Values");
+console.log("Falsy-Values are : \n 1. undefined \n 2. null --> empty \n 3. '0' --> Binary Values \n 4. '' --> Empty Strings \nOtherthan these are TRUTHY Values\n\nExamples");
+
+console.log("1. user = null");
+let user = null;
+if (user) {
+console.log("Condition is true");
+}
+else{
+    console.log("Conditio is False");
+}
+
+console.log("2. user = 'null'");
+let user1 = "null";
+if (user1) {
+console.log("Condition is true");
+}
+else{
+    console.log("Conditio is False");
+}
+
+console.log("3. user = '' ");
+let user2 = "";
+if (user2) {
+console.log("Condition is true");
+}
+else{
+    console.log("Conditio is False");
+}
