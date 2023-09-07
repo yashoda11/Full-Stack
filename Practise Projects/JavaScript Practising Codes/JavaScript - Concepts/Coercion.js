aece0f45772e5265acbfeb125821509baa19f294
Yashoda <yashodalakshmi999@gmail.com>
2023-09-07 22:27:43 +0530

    Coersion

--- Practise Projects/JavaScript Practising Codes/JavaScript - Concepts/Coercion.js
+++ Practise Projects/JavaScript Practising Codes/JavaScript - Concepts/Coercion.js
@@ -1,4 +1,26 @@
-console.log(2+2);
-console.log(2+"2");
-console.log(2+parseInt("2"));
-console.log("2"+"2");
+console.log('\n2 + 2 :', 2+2);
+console.log('2 + parseFloat("2.6") : ', 2+parseFloat("2.6"));
+console.log('2 + parseInt("2.6") : ', 2+parseInt("2.6"));
+console.log('2 + "2" : ', 2+"2");
+console.log('"2" + 2 : ', "2"+2);
+console.log('"2" + "2" : ', "2"+"2");
+
+console.log("\n\nExample for '=='");
+let user = "2";
+
+if(2 == user){
+    console.log("Condition is True");
+}
+else{
+    console.log("Condition is False");
+}
+
+console.log("\n\nExample '==='");
+let user1 = "2";
+
+if(2 === user){
+    console.log("Condition is True");
+}
+else{
+    console.log("Condition is False");
+}
