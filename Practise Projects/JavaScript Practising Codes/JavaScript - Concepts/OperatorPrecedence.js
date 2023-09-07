c0d488e3f3d018ad7be106b37bff8dd5bc27f0d7
Yashoda <yashodalakshmi999@gmail.com>
2023-09-07 21:58:37 +0530

    Operator Precedence Code

---
+++ Practise Projects/JavaScript Practising Codes/JavaScript - Concepts/OperatorPrecedence.js
@@ -0,0 +1,16 @@
+// Operators - Discount Percentage
+
+let listingPrice = 799;
+let sellingPrice = 199;
+let discountPercent = ((listingPrice-sellingPrice)/listingPrice)*100;
+console.log("Discount Percentage is: " + discountPercent);
+displayDiscountPercentage = Math.round(discountPercent);
+console.log(displayDiscountPercentage + "% off");
+var result = listingPrice>sellingPrice;
+console.log(result);
+console.log(typeof result);
+
+console.log("Boolean Expression");
+var temp = 20;
+var result = temp<20;
+console.log(result);
