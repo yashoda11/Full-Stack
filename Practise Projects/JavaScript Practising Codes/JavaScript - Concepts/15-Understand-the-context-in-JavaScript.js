6b2deeab47f69042ea96db72760b2dd4e2ec2700
Yashoda <yashodalakshmi999@gmail.com>
2023-09-09 13:27:20 +0530

    16-Code-hoisting-in-JavaScript.js

---
+++ Practise Projects/JavaScript Practising Codes/JavaScript - Concepts/15-Understand-the-context-in-JavaScript.js
@@ -0,0 +1,10 @@
+function sayHello() {
+    console.log("Hello");
+}
+sayHello();
+
+
+sayHello1();
+function sayHello1() {
+    console.log("Hello World");
+}
---
+++ Practise Projects/JavaScript Practising Codes/JavaScript - Concepts/16-Code-hoisting-in-JavaScript.js
@@ -0,0 +1,52 @@
+function tipper1(a){
+    var bill = a;
+    console.log(bill + 5);
+}
+tipper1(5)
+
+
+function tipper2(a){
+    var bill = a;
+    console.log(bill + 5);
+}
+tipper2("5")
+
+
+function tipper3(a){
+    var bill = parseInt(a);
+    console.log(bill + 5);
+}
+tipper3("5")
+
+
+function tipper4(a){
+    var bill = parseInt(a);
+    console.log(bill + 5);
+}
+tipper4("a")
+
+
+function bigTipper(a){
+    var bill = parseInt(a);
+    console.log(bill + 15);
+}
+bigTipper("200")
+
+
+bigTipperr("200")
+function bigTipperr(a){
+    var bill = parseInt(a);
+    console.log(bill + 25);
+}
+
+// bigTipper1("200")
+// var bigTipper1 = function (a){
+//     var bill = parseInt(a);
+//     console.log(bill + 15);
+// }        gives Error
+
+var bigTipper2 = function (a){
+    var bill = parseInt(a);
+    console.log(bill + 115);
+}
+bigTipper2("200")
