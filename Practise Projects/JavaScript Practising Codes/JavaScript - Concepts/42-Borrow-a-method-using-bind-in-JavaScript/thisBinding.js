540e673075872ae12fd3b5829ba9b83c05d2c3f4
Yashoda <yashodalakshmi999@gmail.com>
2023-09-26 13:17:21 +0530

    42-Borrow-a-method-using-bind-in-JavaScript/thisBinding

---
+++ Practise Projects/JavaScript Practising Codes/JavaScript - Concepts/42-Borrow-a-method-using-bind-in-JavaScript/thisBinding.js
@@ -0,0 +1,17 @@
+const hitesh = {
+  firstName: "Hitesh",
+  lastName: "Choudhary",
+  role: "Admin",
+  courseCount: 3,
+  getInfo: function () {
+    console.log(`
+        First name is ${this.firstName}
+        Last name is ${this.lastName}
+        His role is ${this.role}
+        and he is studying ${this.courseCount} courses
+        `);
+  },
+};
+
+
+hitesh.getInfo();
