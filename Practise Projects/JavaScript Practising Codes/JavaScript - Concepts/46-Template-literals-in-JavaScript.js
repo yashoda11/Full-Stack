a641f7c9b9da6a91ff87d700b87985dd6b4ae09f
Yashoda <yashodalakshmi999@gmail.com>
2023-09-26 14:33:24 +0530

    46-Template-literals-in-JavaScript

--- Practise Projects/JavaScript Practising Codes/JavaScript - Concepts/46-Template-literals-in-JavaScript.js
+++ Practise Projects/JavaScript Practising Codes/JavaScript - Concepts/46-Template-literals-in-JavaScript.js
@@ -1,10 +1,23 @@
-var name = `hitesh`;
-var fullName = `${name} choudhary`;
+var namee = `hitesh`;
+var fullName = `${namee} choudhary`;
 
 console.log(fullName);
+// hitesh choudhary
+
 console.log(fullName.charAt(0));
+//h
+
 console.log(fullName.charAt(10));
+//u
+
 console.log(fullName.endsWith("y"));
+//true
+
 console.log(fullName.endsWith("?"));
+//false
+
 console.log(fullName.includes("hitesh"));
+//true
+
 console.log(fullName.toUpperCase());
+//HITESH CHOUDHARY
