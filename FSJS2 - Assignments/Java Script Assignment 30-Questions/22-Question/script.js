185f63ab7d0637a0b989585966dcdbf7ba4d0179
Yashoda <yashodalakshmi999@gmail.com>
2024-03-23 08:50:34 +0530

    Java Script

--- FSJS2 - Assignments/Java Script Assignment 30-Questions/22-Question/script.js
+++ FSJS2 - Assignments/Java Script Assignment 30-Questions/22-Question/script.js
@@ -1,4 +1,4 @@
-// 22. The following is an array of 10 students ages:
+\]=// 22. The following is an array of 10 students ages:
 //     => const ages = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24]
 //     - Sort the array and find the min and max age
 //     - Find the median age(one middle item or two middle items divided by two)
